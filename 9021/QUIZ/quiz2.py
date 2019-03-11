60146b6786e142492642dedf6dd2034727ad404a
Jie <1306368439@qq.com>
2019-03-11 21:12:12 +0800

    add third question

--- 9021/QUIZ/quiz2.py
+++ 9021/QUIZ/quiz2.py
@@ -19,3 +19,21 @@ def draw_line(rule_nb, first, second, length):
         res.append(rule[res[i - 2], res[i - 1]])
         print(res[i], end='')
     print()
+def uniquely_produced_by_rule(line):
+
+    if (len(line) <= 2):
+        return -1
+    dicall = {}
+    dic = {}
+    for i in range(16):
+        dicall[int(i)] = rule_encoded_by(i)
+    for j in range(len(line) - 2):
+        if ((int(line[j]), int(line[j + 1])) not in dic):
+            dic[int(line[j]), int(line[j + 1])] = int(line[j + 2])
+        elif (dic[int(line[j]), int(line[j + 1])] != int(line[j + 2])):
+            return -1
+    if (len(dic) < 4):
+        return -1
+    else:
+        a, b, c, d = dic[(0, 0)], dic[(0, 1)], dic[(1, 0)], dic[(1, 1)]
+        return(d*1+c*2+b*4+a*8)
